3eff5fa511e88f84ea6fae2f6f379a12acf037f3
Felipe Wilson Viana <167330083+Felipewv93@users.noreply.github.com>
2025-07-11 17:39:38 -0300

    update js

--- script.js
+++ script.js
@@ -2,17 +2,20 @@ function calcularMediaAPs() {
     const APs = document.getElementById('form_aps')
     APs.addEventListener('submit', function(e) {
         e.preventDefault();
+        let erros = []
         let nap1 = document.getElementById('ap1')
         let n1 = parseFloat(nap1.value)
         if (isNaN(n1) || n1 < 0 || n1 > 10) {
-            alert('Por favor, insira uma nota válida para a AP1.')
-            return
+            erros.push('AP1')
         }
         let nap2 = document.getElementById('ap2')
         let n2 = parseFloat(nap2.value)
         if (isNaN(n2) || n2 < 0 || n2 > 10) {
-            alert('Por favor, insira uma nota válida para a AP2.')
-            return
+            erros.push('AP2')
+        }
+        if (erros.length > 0) {
+            alert(`Por favor, insira notas válidas para: ${erros.join(', ')}.`);
+            return;
         }
         let med_ap = (n1 + n2) / 2
         let ap_pond = med_ap * 0.3
@@ -27,27 +30,29 @@ function calcularMediaPAI() {
     const PAI = document.getElementById('form_pai')
     PAI.addEventListener('submit', function(e) {
         e.preventDefault();
+        let erros = []
         let notas_pai = []
         let npai1 = document.getElementById('pai1')
         let p1 = parseFloat(npai1.value)
         if (isNaN(p1) || p1 < 0 || p1 > 50) {
-            alert('Por favor, insira uma nota válida para a PAI1.')
-            return
+            erros.push('PAI 1')
         }
-        notas_pai.push(p1 / 5)
         let npai2 = document.getElementById('pai2')
         let p2 = parseFloat(npai2.value)
         if (isNaN(p2) || p2 < 0 || p2 > 100) {
-            alert('Por favor, insira uma nota válida para a PAI2.')
-            return
+            erros.push('PAI 2')
         }
-        notas_pai.push(p2 / 10)
         let npai3 = document.getElementById('pai3')
         let p3 = parseFloat(npai3.value)
          if (isNaN(p3) || p3 < 0 || p3 > 100) {
-            alert('Por favor, insira uma nota válida para a PAI3.')
-            return
+            erros.push('PAI 3')
         }
+        if (erros.length > 0) {
+            alert(`Por favor, insira notas válidas para: ${erros.join(', ')}.`);
+            return;
+        }
+        notas_pai.push(p1 / 5)
+        notas_pai.push(p2 / 10)
         notas_pai.push(p3 / 10)
         notas_pai.sort((a, b) => a - b)
         notas_pai.shift()
@@ -64,19 +69,22 @@ function quantoFalta() {
     const PN = document.getElementById('form_np')
     PN.addEventListener('submit', function(e) {
         e.preventDefault();
+        let erros = []
         let m_ap = document.getElementById('maps')
         let map = parseFloat(m_ap.value)
         if (isNaN(map) || map < 0 || map > 10) {
-            alert('Por favor, insira uma nota válida para a média das APs.')
-            return
+            erros.push('Média das APs')
         }
-        let pond_ap = map * 0.3
         let m_pai = document.getElementById('mpai')
         let pai = parseFloat(m_pai.value)
         if (isNaN(pai) || pai < 0 || pai > 10) {
-            alert('Por favor, insira uma nota válida para a média das PAI.')
-            return
+            erros.push('Média da PAI')
         }
+        if (erros.length > 0) {
+            alert(`Por favor, insira notas válidas para: ${erros.join(', ')}.`);
+            return;
+        }
+        let pond_ap = map * 0.3
         let pond_pai = pai * 0.3
         let qf = (6 - (pond_ap + pond_pai)) / 0.4
         let np = document.getElementById('np_res')
@@ -86,7 +94,6 @@ function quantoFalta() {
 }
 
 function limparAP() {
-
     document.getElementById('ap_res').innerHTML = ''
     document.getElementById('ap_res').style.display = 'none'
 }
@@ -97,7 +104,6 @@ function limparPAI() {
 }
 
 function limparNP() {
-
     document.getElementById('np_res').innerHTML = ''
     document.getElementById('np_res').style.display = 'none'
 }
